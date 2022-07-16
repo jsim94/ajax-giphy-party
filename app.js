11900eb0045587788f0d6f02e7ac9f139a7e8e72
jsim94 <j.sime94@gmail.com>
2022-07-15 22:55:43 -0400

    Init code to meet reqs

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Let's get this party started!");
+function addGif(src) {
+  const newAttr = {
+    class: "p-3 col-12 col-sm-6 col-lg-3 overflow-hidden",
+    style: "height:300px",
+  };
+  const newGif = $("<div>", newAttr).append($("<img>", { src: src, class: "w-100 overflow-hidden" }));
+  $("#gallery").append(newGif);
+}
+
+function getRandomGif(gifs) {
+  const randI = Math.floor(Math.random() * 50);
+  addGif(gifs[randI].images.original.url);
+}
+
+function removeImages() {
+  $("#gallery").empty();
+}
+
+async function getNewImage(search) {
+  try {
+    const res = await axios.get("https://api.giphy.com/v1/gifs/search", {
+      params: { api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", q: search },
+    });
+    getRandomGif(res.data.data);
+  } catch (e) {
+    $("#warningModal").modal("show");
+  }
+}
+
+$("#submit").on("click", (e) => {
+  e.preventDefault();
+  getNewImage($("#input").val());
+});
+
+$("#remove").on("click", (e) => {
+  e.preventDefault();
+  removeImages();
+});
